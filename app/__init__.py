27e3bc868f6e8160ad110ffd1c89697a6b302036
Vignesh <garrapallyvignesh8055@gmail.com>
2023-12-02 16:37:05 +0530

    Phase 2 Done

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,8 @@ from flask import Flask
 
 def create_app():
     app = Flask(__name__)
-    app.config['IMAGE_EXTS'] = [".png", ".jpg", ".jpeg", ".gif", ".tiff"]
+    app.secret_key = 'a1b2c3d4e5f6g7h8i9j0'
+    app.config['IMAGE_EXTS'] = [".png", ".jpg", ".heic"]
     from .routes import main
     app.register_blueprint(main)
 
--- app/routes.py
+++ app/routes.py
@@ -7,10 +7,26 @@ Attributes:
 
 from pathlib import Path
 import binascii
-from flask import Blueprint, render_template, current_app, send_from_directory
+import os
+import pickle
+from flask import Blueprint, render_template, current_app, send_from_directory,flash,redirect,url_for
+import faiss
+import numpy as np
+import torch
+from transformers import AutoImageProcessor, AutoModel
+from PIL import Image
+from flask import request, jsonify
+from werkzeug.utils import secure_filename
+from pillow_heif import register_heif_opener
 
-
+register_heif_opener()
 main = Blueprint('main', __name__)
+
+device = torch.device('cuda' if torch.cuda.is_available() else "cpu")
+processor = AutoImageProcessor.from_pretrained('facebook/dinov2-small')
+model = AutoModel.from_pretrained('facebook/dinov2-small').to(device)
+model.eval()
+
 
 @main.route('/')
 def index():
@@ -81,5 +97,61 @@ def gallery():
 
 @main.route('/search')
 def search():
-    # Logic for the search page
     return render_template('search.html')
+
+
+def perform_search(image_path, num_results):
+
+    index = faiss.read_index('vector.index')
+    image_list = pickle.load(open('images.pkl', 'rb'))
+
+    # Load the model and processor
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    processor = AutoImageProcessor.from_pretrained('facebook/dinov2-small')
+    model = AutoModel.from_pretrained('facebook/dinov2-small').to(device)
+    model.eval()
+
+    # Load and process the image
+    image = Image.open(image_path).convert('RGB')
+    inputs = processor(images=image, return_tensors="pt").to(device)
+
+    # Extract features
+    with torch.no_grad():
+        outputs = model(**inputs)
+    
+    # Normalize the features
+    embeddings = outputs.last_hidden_state.mean(dim=1)
+    vector = embeddings.detach().cpu().numpy()
+    vector = np.float32(vector)
+    faiss.normalize_L2(vector)
+
+    # Read the index and perform the search
+    
+    distances, indices = index.search(vector, num_results)
+    encoded_paths = [encode(image_list[idx]) for idx in indices[0]]
+    distances = distances[0].tolist()
+    
+    return list(zip(encoded_paths, distances))
+
+@main.route('/perform_search', methods=['POST'])
+def search_route():
+    # Check if the index files exist
+    if not os.path.exists('vector.index') or not os.path.exists('images.pkl'):
+        flash('Please sync your images before searching.')
+        return redirect(url_for('main.index'))
+
+    # Process the uploaded image
+    image_file = request.files['image']
+    num_results = request.form.get('num_results', 3, type=int)
+    if image_file:
+        filename = secure_filename(image_file.filename)
+        image_path = os.path.join('query_images', filename)
+        #create query_images folder if it does not exist
+        if not os.path.exists('query_images'):
+            os.makedirs('query_images')
+        image_file.save(image_path)
+
+        # Perform the search
+        search_results = perform_search(image_path, num_results)
+        print(search_results)
+        return render_template('results.html', search_results=search_results)
